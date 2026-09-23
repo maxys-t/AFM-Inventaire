41a5c9a3cbfa47a9c3ea7660289ec6b0402f6522
Maxys AFM <maxys@accessflow.fr>
2026-09-23 17:26:57 +0200

    v1.9.2 - Update APP_URL to point to the new site address

--- js/config.js
+++ js/config.js
@@ -17,7 +17,7 @@ const SUPABASE_KEY = "sb_publishable_s9Yip985Howj4yDog-2TTA_ufNaQPVk";
 /* --- Adresse du site (optionnel) ------------------------------
    Laisser vide : les QR codes utilisent l'adresse courante.
    -------------------------------------------------------------- */
-const APP_URL = "";
+const APP_URL = "https://inventaire.accessflow.fr/";
 
 /* --- Catégories de matériel -----------------------------------
    Deux niveaux, tous deux obligatoires à la création d'un item.
